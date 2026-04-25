60e6d640bdd7ab70a3161cd1788d26a127eb10bf
lowpolyphosphorus <158785409+LowPolyPhosphorus@users.noreply.github.com>
2026-04-24 18:26:01 -0700

    Add UNLOCK_DURATION to secrets.example.py

--- secrets.example.py
+++ secrets.example.py
@@ -1,5 +1,4 @@
 # Copy this file to secrets.py and fill in your details
-
 WIFI_SSID = "your_wifi_ssid"
 WIFI_PASSWORD = "your_wifi_password"
-UNLOCK_PASSWORD = "your_unlock_password"
+UNLOCK_DURATION = 2  # seconds the relay stays open
